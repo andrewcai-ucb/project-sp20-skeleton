cb788bccb8a76745e9854afc5c4da9189b0c8f92
Andrew Cai <andrewcai@berkeley.edu>
2020-04-30 22:41:54 -0700

    seems okay

--- solver.py
+++ solver.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from parse import read_input_file, write_output_file
+from parse import read_input_file, write_output_file, read_output_file
 from utils import is_valid_network, average_pairwise_distance, average_pairwise_distance_fast
 import sys
 
@@ -15,35 +15,15 @@ def solve(G):
     Returns:
         T: networkx.Graph
     """
-
-    M = nx.minimum_spanning_tree(G)
     
-    def min_routing_cost_tree(r):
+    def dijkstra_tree(r):
  
-        T = nx.DiGraph()
+        T = nx.Graph()
         T.add_nodes_from(G)
         for p in nx.single_source_dijkstra_path(G, r).values():
             nx.add_path(T, p)
-        for (u, v) in T.edges():
-            T[u][v]['weight'] = G[u][v]['weight']
-        
         return T
         
-    def weighted_bfs_tree(r):
- 
-        T = nx.bfs_tree(G, r)
-        for (u, v) in T.edges():
-            T[u][v]['weight'] = G[u][v]['weight']
-        
-        return T
-        
-    def weighted_mst_tree(r):
- 
-        T = nx.bfs_tree(M, r)
-        for (u, v) in T.edges():
-            T[u][v]['weight'] = G[u][v]['weight']
-        
-        return T
     
     def pruned_tree(T):  
         """Removes edges from a spanning tree to obtain a dominating tree.
@@ -52,32 +32,58 @@ def solve(G):
             r: Root
 
         """    
+        for (u, v) in T.edges():
+            T[u][v]['weight'] = G[u][v]['weight']
+            
         opt_cost = average_pairwise_distance_fast(T.to_undirected())
         
         leaves = PriorityQueue()
         for l in T:
-            if ((T.in_degree(l) == 1) and (T.out_degree(l) == 0)):
-                leaves.put(l, -G[l][list(T.pred[l])[0]]['weight'])
+            if T.degree(l) == 1:
+                leaves.put((-G[l][list(T[l])[0]]['weight'], l))
                 
         while not leaves.empty():
-            v = leaves.get()
-            S = T.to_undirected()
-            S.remove_node(v)
-            c = average_pairwise_distance_fast(S)
-            if ((c < opt_cost) and is_valid_network(G, S)):
-                opt_cost = c
-                if T.in_degree(v) == 1:
-                    w = T.pred[v];
-                    if T.in_degree(w) == 1:
-                        leaves.put(w, -G[w][T.pred[l].keys[0]]['weight'])
-                T.remove_node(v)  
+            v = leaves.get()[1]
+            if T.degree(v) == 1:
+                w = list(T[v])[0]
+                T.remove_node(v)
+                if not is_valid_network(G, T):
+                    T.add_edge(v, w, weight=G[v][w]['weight'])
+                else:
+                    c = average_pairwise_distance_fast(T)
+                    if c < opt_cost:
+                        opt_cost = c
+                        if T.degree(w) == 1:
+                            leaves.put((-G[w][list(T[w])[0]]['weight'], w))
+                    else: 
+                        T.add_edge(v, w, weight=G[v][w]['weight'])      
+        
+        for l in T:
+            if T.degree(l) == 1:
+                leaves.put((-G[l][list(T[l])[0]]['weight'], l))
                 
-        return T.to_undirected()
+        while not leaves.empty():
+            v = leaves.get()[1]
+            if T.degree(v) == 1:
+                w = list(T[v])[0]
+                T.remove_node(v)
+                if not is_valid_network(G, T):
+                    T.add_edge(v, w, weight=G[v][w]['weight'])
+                else:
+                    c = average_pairwise_distance_fast(T)
+                    if c < opt_cost:
+                        opt_cost = c
+                        if T.degree(w) == 1:
+                            leaves.put((-G[w][list(T[w])[0]]['weight'], w))
+                    else: 
+                        T.add_edge(v, w, weight=G[v][w]['weight'])      
+                
+        return T
     
-    return min([pruned_tree(min_routing_cost_tree(v)) for v in G] + 
-        [pruned_tree(weighted_bfs_tree(v)) for v in G] + 
-        [pruned_tree(weighted_mst_tree(v)) for v in G], key=average_pairwise_distance_fast)
-
+    return min([pruned_tree(dijkstra_tree(v)) for v in G] + 
+        [pruned_tree(nx.bfs_tree(G, v).to_undirected()) for v in G] + 
+        [pruned_tree(nx.minimum_spanning_tree(G)) for v in G], key=average_pairwise_distance_fast)
+        
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
@@ -88,6 +94,6 @@ if __name__ == '__main__':
     G = read_input_file(path)
     T = solve(G)
     assert is_valid_network(G, T)
-    print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
+    print("Average  pairwise distance: {}".format(average_pairwise_distance_fast(T)))
     write_output_file(T, 'test.out')
     
